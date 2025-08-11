05e63a1c021d016eb0764e99d7261b0f4cb3f623
msmit-CN <158488005+msmit-CN@users.noreply.github.com>
2025-08-11 13:12:50 +0200

    feat: schema validation items for aks and resource_group_name change (#128)
    * feat: schema validation items for aks and resource_group_name change
    
    * deleted lock file
    
    * feat: revert resource_group_name change
    
    * feat: update examples resource_group proporty of aks
    
    * feat: remove temporary_name_for_rotation from example
    
    * feat: network security group property
    
    * fix: correct resource_group property naming in network and aks modules
    
    * fix: enhance identity output logic to check for identity_ids length

--- examples/maintenance/main.tf
+++ examples/maintenance/main.tf
@@ -1,6 +1,6 @@
 module "naming" {
   source  = "cloudnationhq/naming/azure"
-  version = "~> 0.1"
+  version = "~> 0.24"
 
   suffix = ["demo", "dev"]
 }
@@ -19,20 +19,20 @@ module "rg" {
 
 module "kv" {
   source  = "cloudnationhq/kv/azure"
-  version = "~> 2.0"
+  version = "~> 4.0"
 
   naming = local.naming
 
   vault = {
-    name           = module.naming.key_vault.name_unique
-    location       = module.rg.groups.demo.location
-    resource_group = module.rg.groups.demo.name
+    name                = module.naming.key_vault.name_unique
+    location            = module.rg.groups.demo.location
+    resource_group_name = module.rg.groups.demo.name
   }
 }
 
 module "aks" {
   source  = "cloudnationhq/aks/azure"
-  version = "~> 3.1"
+  version = "~> 3.0"
 
   keyvault = module.kv.vault.id
 
